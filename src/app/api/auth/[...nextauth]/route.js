723ce2be08a369f63348692d31dd810a42822ee5
levelp <onojasteven13@gmail.com>
2026-06-22 19:58:47 +0100

    Pusha T

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,20 +1,26 @@
-import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
 
 const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        }),
-    ],
-    callbacks: {
-        async session({ session, user, token }) {
-          session.user.username = session.user.name.split(' ').join('').toLocaleLowerCase()
-          session.user.uid = token.sub
-          return session
-        }
-      }
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+
+  secret: process.env.NEXTAUTH_SECRET,
+
+  callbacks: {
+    async session({ session, token }) {
+      const name = session?.user?.name || ""
+
+      session.user.username = name.split(" ").join("").toLowerCase()
+      session.user.uid = token?.sub || null
+
+      return session
+    },
+  },
 })
 
-export { handler as GET, handler as POST}
+export { handler as GET, handler as POST }
